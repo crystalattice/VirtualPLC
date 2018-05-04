dd6ec083f6126b9b5f58139181e1792c2215c840
crystalattice <crystalattice@gmail.com>
2018-05-04 11:53:39 -0500

    Completed calculation of gravity fluid flow

--- utility_formulas.py
+++ utility_formulas.py
@@ -4,15 +4,24 @@ import math
 
 
 def gravity_flow_rate(diameter, slope, rough_coeff=140):
-    """Calculates fluid flow due to gravity.
+    """Calculates approximate fluid flow due to gravity.
+
+    Should be within 5% of actual value.
 
     Based on the Hazen-Williams equation (https://en.wikipedia.org/wiki/Hazen–Williams_equation). Assumes a 2 inch,
     polyethylene pipe.
+
+    :param diameter: Pipe diameter, in inches
+    :param slope: Slope of pipe, from reservoir to measure point
+    :param rough_coeff: Roughness coefficient of pipe
+
+    :return: Approximate fluid flow rate, in gpm
     """
     coeff = math.pow(rough_coeff, 1.852)
     diam = math.pow(diameter, 4.8704)
-    flow = ((coeff * diam * slope) / 4.52) ** (1.0 / 1.852)
-    return flow
+    root_flow = math.sqrt(((coeff * diam * slope) / 4.52))
+    return root_flow
 
 
-print(gravity_flow_rate(2, 0.6))
+if __name__ == "__main__":
+    print(gravity_flow_rate(2, 0.6))
