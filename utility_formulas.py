a26f3740380d8d563d9c74d2a5f1742294fc8cc1
crystalattice <crystalattice@gmail.com>
2018-05-07 08:48:26 -0500

    Added pressure/head conversion functions

--- utility_formulas.py
+++ utility_formulas.py
@@ -2,6 +2,11 @@
 
 import math
 
+GRAVITY = 32.174  # ft/s^2
+WATER_SPEC_WEIGHT = 62.4  # lb/ft^3
+WATER_DENSITY = 1.94  # slugs/ft^3
+WATER_SPEC_GRAV = 1.0
+
 
 def gravity_flow_rate(diameter, slope, rough_coeff=140):
     """Calculates approximate fluid flow due to gravity.
@@ -23,7 +28,7 @@ def gravity_flow_rate(diameter, slope, rough_coeff=140):
     return root_flow
 
 
-def static_head(height):
+def static_press(height):
     """Calculate the pressure due to height of fluid.
 
     :param height: Fluid height, in feet
@@ -34,6 +39,44 @@ def static_head(height):
     return press
 
 
+def alt_static_press(height, density=WATER_DENSITY):
+    """Calculate static pressure for any fluid.
+
+    :param height: Fluid height, in feet
+    :param density: Fluid density. Default assumes water.
+
+    :return: Fluid pressure, in psi
+    """
+    press = density * GRAVITY * height
+    return press
+
+
+def press_to_head(press, spec_grav=WATER_SPEC_GRAV):
+    """Calculate fluid head from pressure.
+
+    :param press: Fluid pressure, in psi
+    :param spec_grav: Specific gravity of fluid
+
+    :return: Fluid head, in feet
+    """
+    head = (74.215 * press) / (spec_grav * GRAVITY)
+    return head
+
+
+def head_to_press(head, spec_grav=WATER_SPEC_GRAV):
+    """Calculate pressure from fluid head.
+
+    :param head: Fluid head, in feet
+    :param spec_grav: Specific gravity of fluid
+
+    :return: Fluid pressure, in psi
+    """
+    press = (spec_grav * GRAVITY * head) / 74.215
+    return press
+
+
 if __name__ == "__main__":
     print(gravity_flow_rate(2, 0.6))
     print(static_head(150))
+    print(press_to_head(65.0))
+    print(head_to_press(150))
--- valve/tests/test_functional.py
+++ valve/tests/test_functional.py
@@ -1,5 +1,5 @@
 from valve.valve import Gate, Globe, Relief
-from utility_formulas import gravity_flow_rate
+import utility_formulas
 
 """Assumes valves in series, with the first supplied by a tank 6 feet above the valve with a pipe length of 10 feet.
 
@@ -10,31 +10,50 @@ Gravity flow rate from http://www.calctool.org/CALC/eng/civil/hazen-williams_g
 class GravityFlow:
     @staticmethod
     def test_grav_flow():
-        flow_rate = gravity_flow_rate(2, 0.6)
+        flow_rate = utility_formulas.gravity_flow_rate(2, 0.6)
         assert flow_rate == 191.3767672898554
+
+    @staticmethod
+    def test_static_press():
+        press = utility_formulas.static_head(10)
+        assert press == 4.329004329004329
 
 
 class TestFirstValve:
+    def test_input_press(self):
+        valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=utility_formulas.gravity_flow_rate(2, 0.6),
+                      press_in=utility_formulas.static_head(10))
+        assert valve1.press_in == 4.329004329004329
+
     def test_input_flow(self):
-        valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=gravity_flow_rate(2, 0.6), press_in=2.6)
-        assert valve1.press_in == 2.6
+        valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=utility_formulas.gravity_flow_rate(2, 0.6),
+                      press_in=utility_formulas.static_head(10))
+        assert valve1.flow_in == 191.3767672898554
 
     def test_press_drop(self):
-        valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=48, press_in=2.6)
+        valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=utility_formulas.gravity_flow_rate(2, 0.6),
+                      press_in=utility_formulas.static_head(10))
         press_diff = valve1.press_drop(valve1.flow_in)
         assert press_diff == 0.28444444444444444
 
     def test_output_flow(self):
-        valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=48, press_in=2.6)
-        out_flow = valve1.get_press_out(valve1.press_in)
+        valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=utility_formulas.gravity_flow_rate(2, 0.6),
+                      press_in=utility_formulas.static_head(10))
+        out_flow = valve1.sys_flow_rate(valve1.Cv, valve1.deltaP)
         assert out_flow == 2.3155555555555556
+
+    def test_press_out(self):
+        valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=utility_formulas.gravity_flow_rate(2, 0.6),
+                      press_in=utility_formulas.static_head(10))
+        press_out = valve1.press_in - valve1.press_drop(valve1.flow_in)
+        assert press_out == 4.0445598845598845
 
 
 class TestSecondValve:
-    def test_input_flow(self):
+    def test_input_press(self):
         valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=48, press_in=2.6)
-        valve2 = Globe("Valve 2", )
-        assert valve1.press_in == 2.6
+        valve2 = Globe("Valve 2", sys_flow_in=valve1.flow_out, press_in=valve1.press_out)
+        assert valve2.press_in == 4.0445598845598845
 
     def test_press_drop(self):
         valve1 = Gate("Valve 1", flow_coeff=90, sys_flow_in=48, press_in=2.6)
