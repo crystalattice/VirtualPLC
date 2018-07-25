cb08960e0423162950f52aff4ac98a923a01a411
crystalattice <crystalattice@gmail.com>
2018-07-25 10:41:18 -0500

    Added ToggleButton

--- Models/FuelFarm/hmi/hmi.py
+++ Models/FuelFarm/hmi/hmi.py
@@ -1,8 +1,16 @@
+import kivy
+kivy.require("1.10.0")
+
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
+from kivy.config import Config
+
+Config.set("graphics", "width", "1062")
+Config.set("graphics", "height", "849")
 
 
-class Background(ScreenManager):
+class Background(Widget):
     pass
 
 
