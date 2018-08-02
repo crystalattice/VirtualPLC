0e6cd68a3db8649a8e75d3248b8f4f5daae1a5e5
crystalattice <crystalattice@gmail.com>
2018-08-02 08:09:02 -0500

    code cleanup

--- Models/FuelFarm/hmi/hmi.py
+++ Models/FuelFarm/hmi/hmi.py
@@ -12,24 +12,17 @@ Config.set("graphics", "width", "1062")
 Config.set("graphics", "height", "849")
 Config.set("graphics", "resizable", False)
 
-# print(components.tank1.level)
-# print(components.tank1.static_tank_press)
-
 
 class HMI(FloatLayout):
 
     @staticmethod
     def on_state(valve):  # Get the status of the valve
-        # if valve.group == "valve1":
         if valve.state == "down":
             # print(valve.group, "Opened")
-            exec("functionality.{}_open()".format(valve.group))  # Dynamically call valve function
-            # functionality.gate1_open()
+            exec("functionality.{}_open()".format(valve.group))  # Dynamically call valve open
         else:
             # print(valve.group, "Closed")
-            exec("functionality.{}_close()".format(valve.group))
-
-
+            exec("functionality.{}_close()".format(valve.group))  # Dynamically call valve close
 
 
 class HMIApp(App):
